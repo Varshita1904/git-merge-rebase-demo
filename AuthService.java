934504b3047f4a219c6abdbafad762c1f16d20ec
Varshita1904 <155021724+Varshita1904@users.noreply.github.com>
2026-01-27 18:34:03 -0600

    Update AuthService.java

--- AuthService.java
+++ AuthService.java
@@ -1,5 +1,8 @@
 public class AuthService {
     public boolean login(String user, String password) {
+        if(user == null || password == null) {
+            return false;
+        }
         return true;
     }
 }
